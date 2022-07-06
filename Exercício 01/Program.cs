9900cc6d70105114dd840e87d776f3e16c318535
curso.ads <curso.ads@LAB06-07>
2022-07-06 18:20:18 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício 01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2;
+            double media;
+
+            Console.WriteLine("Digite a primeira Nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a Segunda Nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2)/2;
+
+
+            if (media >= 8.5)
+            {
+                Console.WriteLine ("Conceito : A");
+            }
+            else if (media >=7.0 && media < 8.4)
+            {
+                Console.WriteLine("Conceito : B");
+            }
+            else if (media >=6.0 && media < 6.9)
+            {
+                Console.WriteLine("Conceito : C");
+            }
+            else if (media >= 0.1 && media < 5.9)
+            {
+                Console.WriteLine("Conceito : D");
+            }
+            else if (media >= 0.0)
+            {
+                Console.WriteLine("Conceito : E");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
